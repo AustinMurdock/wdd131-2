5ca158d51b8ae4741ed8a4cffb033b8022e5c93e
Austin Murdock <aumu870@gmail.com>
2025-09-22 18:34:35 -0600

    w01&w02ponder js variables & DOM additions

--- ponder/meta-css/scripts/script.js
+++ ponder/meta-css/scripts/script.js
@@ -16,3 +16,25 @@ area = radius * radius * PI;
 
 console.log("radius:", radius);
 console.log("area:", area);
+
+
+/* example code */
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+// console.log(student); //does not work, can't access a block variable outside the block
+
+/* example code */
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+    // document.getElementsByTagName(h1).style.backgroundColor = "#fff";
+    if (codeValue != "") {
+        document.getElementById(codeValue).style.backgroundColor = "#ffffaa";
+    }
+})
