f5dcc7c6b0e0668eb6f7db652681a640d9e62d33
Austin Murdock <aumu870@gmail.com>
2025-10-08 23:49:57 -0600

    completed w04ponder for w03ponder-responsive

--- ponder/w03-responsive/scripts/script.js
+++ ponder/w03-responsive/scripts/script.js
@@ -1,15 +1,7 @@
 const menuBtn = document.querySelector(".menu-btn");
-let navAs = document.querySelector("nav").querySelectorAll("a");
+let nav = document.querySelector("nav");
 
-menuBtn.addEventListener("click", function(){
-    if (navAs[0].style.display == "block") {
-        navAs.forEach(e=>{
-            e.style.display = "none";
-        });
-    }
-    else {
-        navAs.forEach(e=>{
-            e.style.display = "block";
-        });
-    }
+menuBtn.addEventListener("click", function() {
+    nav.classList.toggle("hidden");
+    menuBtn.classList.toggle("change");
 })
