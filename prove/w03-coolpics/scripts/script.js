4faa159256e7b77a412a4d80f1772b9c3ae487a1
Austin Murdock <aumu870@gmail.com>
2025-10-11 19:25:05 -0600

    restruct modal img replacement

--- prove/w03-coolpics/scripts/script.js
+++ prove/w03-coolpics/scripts/script.js
@@ -1,10 +1,8 @@
 
 const navBtn = document.getElementById("nav-btn");
 const nav = document.querySelector("nav");
-
 const gallery = document.getElementById("gallery-grid");
 const modal = document.getElementById("modal");
-const modalImg = modal.querySelector("img");
 
 navBtn.addEventListener("click", function(){
     nav.classList.toggle("hidden");
@@ -14,14 +12,23 @@ gallery.addEventListener("click", e => {
     // console.log(e);
     clickTarget = e.target;
     if (clickTarget.localName == "img") {
-        modalImg.setAttribute("src", clickTarget.src.replace("-sm", "-full"));
-        modalImg.setAttribute("alt", clickTarget.alt);
+        if (modal.querySelector("img")) {
+            modal.removeChild(modal.querySelector("img"));
+        };
+        modal.appendChild(viewerTemplate(clickTarget));
         modal.showModal();
     }
-})
+});
 modal.addEventListener("click", e => {
     // console.log(e);
     if (e.target.localName != "img") {
         modal.close();
-    }
-})
+    };
+});
+
+function viewerTemplate(img) {
+    let node = document.createElement("img");
+    node.setAttribute("src", img.getAttribute("src").replace("-sm", "-full"));
+    node.setAttribute("alt", img.getAttribute("alt"));
+    return node;
+};
