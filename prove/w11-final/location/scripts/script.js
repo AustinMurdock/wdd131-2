7adf5c3b8f8005a039cda3e15b8c918879d9c545
Austin Murdock <aumu870@gmail.com>
2025-12-15 23:55:47 -0700

    final mostly complete-ish, good enough for now

---
+++ prove/w11-final/location/scripts/script.js
@@ -0,0 +1,73 @@
+
+const pokeApi = "https://pokeapi.co/api/v2/";
+const url = new URL(window.location.href);
+const query = url.searchParams.get("p");
+const pokeImg_dom = document.getElementById("main-img");
+const locations_dom = document.getElementById("locations");
+let poke = {};
+let genArr = [];
+
+async function pCall(query) {
+    const p = await fetch(pokeApi + query);
+    if (p.ok) {
+        let pJson = await p.json();
+        return pJson;
+    }
+    return;
+}
+
+async function pGetJson(id) {
+    const poke = await pCall("pokemon/" + id);
+    return poke;
+}
+
+async function pBuildArr(id) {
+    const areaArr = await pCall("pokemon/" + id + "/encounters");
+    let genArr = [];
+    areaArr.forEach(a => {
+        a.version_details.forEach(v => {
+            let area = a.location_area.name;
+            let gen = v.version.name;
+            if (!genArr[gen]) {
+                genArr[gen] = [];
+            }
+            genArr[gen].push(area);
+        });
+    });
+    return genArr;
+}
+
+function buildListing(gen, areaArr) {
+    let html = `\
+        <h2>${gen}</h2>
+        <ul>
+    `
+    areaArr.forEach(a => {
+        html += `<li>${a}</li>`
+    });
+    html += `</ul>`;
+    return html;
+}
+
+console.log(query);
+pGetJson(query)
+    .then(json => {
+        poke = json;
+        pokeImg_dom.setAttribute("src", poke.sprites.other["official-artwork"].front_default);
+        pokeImg_dom.setAttribute("alt", poke.name + " official artwork");
+            return;
+    });
+pBuildArr(query)
+    .then(arr => {
+        genArr = arr;
+        Object.keys(genArr).forEach((g, i) => {
+            locations_dom.innerHTML += buildListing(g, genArr[g]);
+        });
+        if (Object.keys(genArr).length == 0) {
+            locations_dom.innerHTML += `\
+                <p>
+                    - No location data found for ${String(poke.name).charAt(0).toUpperCase() + String(poke.name).slice(1)}. - \
+                `;
+        }
+        return;
+    });
--- prove/w11-final/scripts/script.js
+++ prove/w11-final/scripts/script.js
@@ -1,6 +1,6 @@
 
 const pokeApi = "https://pokeapi.co/api/v2/";
-const results = document.getElementById("results");
+const results_dom = document.getElementById("results");
 let pokeArr = [];
 
 function buildRow(poke) {
@@ -10,9 +10,20 @@ function buildRow(poke) {
         <td>
         <img src="${poke.sprites.front_default}" alt="${poke.name} sprite">
         </td>
-        <td>${String(poke.name).charAt(0).toUpperCase() + String(poke.name).slice(1)}</td>
-        <td>${poke.types[0].type.name}</td>
-        <td>${poke.types[1] ? poke.types[1].type.name : ""}</td>
+        <td>
+            <a href="./location/index.html?p=${poke.id}">
+                ${String(poke.name).charAt(0).toUpperCase() + String(poke.name).slice(1)}
+            </a>
+        </td>
+        <td class="t-type">
+            <p>${poke.types[0].type.name}</p>
+            <div class="p-type-${poke.types[0].type.name}"></div>
+        </td>
+        <td class="t-type">${poke.types[1] ?
+            "<p>" + poke.types[1].type.name + "</p>" +
+            "<div class=\"p-type-" + poke.types[1].type.name + "\"></div>"
+            : ""}
+        </td>
         <td>${poke.stats[0].base_stat}</td>
         <td>${poke.stats[1].base_stat}</td>
         <td>${poke.stats[2].base_stat}</td>
@@ -20,21 +31,21 @@ function buildRow(poke) {
         <td>${poke.stats[4].base_stat}</td>
         <td>${poke.stats[5].base_stat}</td>
         <td>${poke.stats[6].base_stat}</td>
-    </tr>\
-    `;
+        </tr>\
+        `;
 }
 
 function display(arr) {
-    results.innerHTML = "";
+    results_dom.innerHTML = "";
     console.log(arr);
     arr.forEach(p => {
-        results.innerHTML += buildRow(p)
+        results_dom.innerHTML += buildRow(p)
     });
     return;
 }
 
-async function pCall(item) {
-    const p = await fetch(pokeApi + item);
+async function pCall(query) {
+    const p = await fetch(pokeApi + query);
     if (p.ok) {
         let pJson = await p.json();
         if (pJson.stats) {
@@ -45,13 +56,13 @@ async function pCall(item) {
                 }
             });
         }
-        if (pJson.location_area_encounters) {
+        /* if (pJson.location_area_encounters) {
             const pL = await fetch(pJson.location_area_encounters);
             if (pL.ok) {
                 const pLJson = await pL.json();
                 pJson.location_area_encounters = pLJson;
             }
-        }
+        } */
         // console.log(pJson);
         return pJson;
     }
@@ -66,24 +77,69 @@ async function pBuildArr(start, end) {
     return arr;
 }
 
+function comparator(a, b, direction) {
+    if (a > b) {
+        return direction;
+    }
+    else if (a < b) {
+        return -direction;
+    }
+    else {
+        return 0;
+    }
+}
+
 document.querySelectorAll("th").forEach(th => {
     th.addEventListener("click", e => {
-        pokeArr.sort((a, b) => {
-            if (a.stats[6].base_stat > b.stats[6].base_stat) {
-                return -1;
-            }
-            else if (a.stats[6].base_stat < b.stats[6].base_stat) {
-                return 1
-            }
-            else {
-                return 0;
-            }
-        });
+        switch (e.target.innerText) {
+            case "#":
+                pokeArr.sort((pokeA, pokeB) =>
+                    comparator(pokeA.id, pokeB.id, -1));
+                break;
+                case "":
+                case "Name":
+                    pokeArr.sort((pokeA, pokeB) =>
+                        comparator(pokeA.name, pokeB.name, -1));
+                    break;
+                case "Types":
+                    pokeArr.sort((pokeA, pokeB) =>
+                        comparator(pokeA.types[0].type.name, pokeB.types[0].type.name, -1));
+                    break;
+                case "HP":
+                    pokeArr.sort((pokeA, pokeB) =>
+                        comparator(pokeA.stats[0].base_stat, pokeB.stats[0].base_stat, -1));
+                    break;
+                case "Atk":
+                    pokeArr.sort((pokeA, pokeB) =>
+                        comparator(pokeA.stats[1].base_stat, pokeB.stats[1].base_stat, -1));
+                    break;
+                case "Def":
+                    pokeArr.sort((pokeA, pokeB) =>
+                        comparator(pokeA.stats[2].base_stat, pokeB.stats[2].base_stat, -1));
+                    break;
+                case "SpA":
+                    pokeArr.sort((pokeA, pokeB) =>
+                        comparator(pokeA.stats[3].base_stat, pokeB.stats[3].base_stat, -1));
+                    break;
+                case "SpD":
+                    pokeArr.sort((pokeA, pokeB) =>
+                        comparator(pokeA.stats[4].base_stat, pokeB.stats[4].base_stat, -1));
+                    break;
+                case "Spd":
+                    pokeArr.sort((pokeA, pokeB) =>
+                        comparator(pokeA.stats[5].base_stat, pokeB.stats[5].base_stat, -1));
+                    break;
+                case "Total":
+                    pokeArr.sort((pokeA, pokeB) =>
+                        comparator(pokeA.stats[6].base_stat, pokeB.stats[6].base_stat, -1));
+                    break;
+                }
+            // e.target.innerText += "↑";
         display(pokeArr);
     });
 });
 
-pBuildArr(1, 20)
+pBuildArr(1, 151)
     .then(arr => {
         pokeArr = arr;
         display(pokeArr);
